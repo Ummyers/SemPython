94d5b2d901b1bf271f38affd3f4fec89d4ef993c
ummyers <mu.andrea@ciencias.unam.mx>
2020-04-03 14:57:59 -0600

    Versión secuencial

--- Tarea2/tarea2.c
+++ Tarea2/tarea2.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>  
 #include <math.h>
 
-#define N 1000
+#define N 100000
 
 /* Declaracion de funciones */
 double * generadorAleatorios(double a, double b);
@@ -12,6 +12,7 @@ void monteCarlo(int q, int w, int e, int r, double (*funcion)());
 double masa_1(double x, double y);
 double masa_2(double x, double y);
 /*Funciones de centro de masa */
+
 double centro_1x(double x, double y);
 double centro_1y(double x, double y);
 double centro_2x(double x, double y);
@@ -30,19 +31,21 @@ int main(int argc, char const *argv[])
 
 	int n = N;
 	printf("Con %d puntos los resultados son:\n", n);
+
 	printf("---Masa 1:\n");
 	monteCarlo(0,1,0,1,masa_1); 
 	printf("El centro de masa en x es:\n");
 	monteCarloCentro(0,1,0,1,1.5,centro_1x);
 	printf("El centro de masa con y es:\n");
 	monteCarloCentro(0,1,0,1,1.5,centro_1y);
+
 	printf("---Masa 2:\n");
 	monteCarlo(-1,1,-1,1,masa_2); 
 	printf("El centro de masa en x es:\n");
 	monteCarloCentro(-1,1,-1,1,2.6635,centro_2x);
 	printf("El centro de masa con y es:\n");
 	monteCarloCentro(-1,1,-1,1,2.6635,centro_2y);
-
+	
 	//Tiempo final
 	t2 = MPI_Wtime();
 	printf("El tiempo del programa es: %lf\n", t2-t1);
@@ -60,9 +63,18 @@ en el intervalo [a,b]
 */
 double * generadorAleatorios(double a, double b){
 	static double numAle[N];
-	for (int i = 0; i< N; i++){
+
+	if(a < 0){
+		for (int i = 0; i< N; i++){
+				numAle[i] = drand48() * (b-a) - b; 
+		}
+	}else{
+		for (int i = 0; i< N; i++){
 				numAle[i] = (double) rand()/RAND_MAX*b + a;
+		}
 	}
+
+	
 	return numAle;
 }
 /* Metodo auxiliar para ver el comportamiento de los
@@ -99,6 +111,8 @@ void monteCarlo(int q, int w, int e, int r, double (*funcion)()){
 	// printf("Izquierdo: %lf\n", iz );
 	suma = izq * suma;
 	printf(" La aproximación es: %lf kg\n", suma);
+	suma = 0.0;
+	izq = 0.0;
 }
 
 /* Metodo que dados los limites de una doble integral calcula la 
@@ -119,14 +133,13 @@ void monteCarloCentro(int q, int w, int e, int r, double div, double (*funcion)(
 	double M = 1/div;
 	// printf("M = %lf \n", M);
 	double izq = ((b-a)*(d-c))/N;
-	// printf("Izq = %lf \n", izq);
 
 	for(int i =1; i<=N; i++){
 		suma = suma + funcion(equis[i-1],yes[i-1]);	// suma = suma + masa_1(equis[i-1],yes[i-1]);
 	}
 	// printf("Izquierdo: %lf\n", izq );
 	suma = M*izq*suma;
-	printf(" La aproximación con m %lf es: %lf\n",izq, suma);
+	printf("La aproximación con m %lf es: %lf\n",izq, suma);
 	suma = 0.0;
 	izq = 0.0;
 	M = 0.0;
@@ -138,21 +151,24 @@ double masa_1(double x, double y){
 }
 
 double masa_2(double x, double y){
-	double evalu = sin(sqrt(pow(x,2)+pow(y,2)));
+	// double evalu = sin(sqrt(pow(x,2)+pow(y,2)));
+	double evalu = sin(sqrt(x*x+y*y));
 }
 
 double centro_1x(double x, double y){
-	double evalu = x *(pow(x,2) + (2*x*y));
+	double evalu = x*(pow(x,2) + (2*x*y));
 }
 
 double centro_1y(double x, double y){
-	double evalu = y *((x+y) + (2*pow(y,2)));
+	double evalu = y*((x+y) + (2*y*y));
 }
 
 double centro_2x(double x, double y){
-	double evalu = x*(x*(sin(sqrt(pow(x,2)+pow(y,2)))));
+	double evalu = sqrt((x*x) + (y*y)); 
+    return x * sin(evalu);
 }
 
 double centro_2y(double x, double y){
-	double evalu = y * y * sin(sqrt(pow(x,2)+pow(y,2)));
+	double evalu = sqrt((x*x)+(y*y));
+	return y * sin(evalu);
 }
